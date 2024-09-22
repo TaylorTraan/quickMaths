8a2ac4276d540645fba3e712cc072ac703f81b42
Taylor Tran <taylortraan0709@gmail.com>
2024-09-21 17:34:46 -0700

    Initial Commit

---
+++ Quick Math/Quick_MathApp.swift
@@ -0,0 +1,17 @@
+//
+//  Quick_MathApp.swift
+//  Quick Math
+//
+//  Created by Taylor Tran on 9/21/24.
+//
+
+import SwiftUI
+
+@main
+struct Quick_MathApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
